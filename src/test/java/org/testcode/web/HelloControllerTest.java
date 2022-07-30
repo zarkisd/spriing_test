56de92bfb39d8bfe52560afd9aea2873340684b4
zarkisd <75956855+zarkisd@users.noreply.github.com>
2022-07-30 22:04:01 +0900

    junit test

---
+++ src/test/java/org/testcode/web/HelloControllerTest.java
@@ -0,0 +1,46 @@
+package org.testcode.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+
+    @Test
+    public void hello() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+    @Test
+    public void helloDto() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+        mvc.perform(get("/hello/dto").param("name", name)
+                                               .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect( jsonPath("$.name").value(name))
+                .andExpect( jsonPath("$.amount").value(amount));
+
+    }
+}
---
+++ src/test/java/org/testcode/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,26 @@
+package org.testcode.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_fun_test(){
+        //given
+        String name = "test";
+        int amount= 1000;
+
+        //when
+        HelloResponseDto dto= new HelloResponseDto(name, amount);
+
+        //then
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+
+
+}
